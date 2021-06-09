4d9ebf5abb57cd89d49e6242e0d6820d32132bc7
Aivee Kei Soriano <aiveekei.soriano@gmail.com>
2021-06-09 23:24:12 +0800

    up to delete book

---
+++ src/components/BooksList.jsx
@@ -0,0 +1,93 @@
+import { Button, CircularProgress, Dialog, DialogTitle, IconButton, List, ListItem, ListItemSecondaryAction, ListItemText, makeStyles, Paper, Snackbar, Typography } from "@material-ui/core"
+import { Delete } from "@material-ui/icons"
+import { Alert } from "@material-ui/lab"
+import { useEffect, useState } from "react"
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        padding: theme.spacing(2),
+        marginTop: theme.spacing(3),
+        width: '500px',
+    },
+    header: {
+        padding: theme.spacing(2),
+        paddingBottom: theme.spacing(0)
+    },
+    deleteDialog: {
+        padding: theme.spacing(2),
+    }
+}))
+
+export default function BooksList() {
+
+    let classes = useStyles()
+
+    let [loading, setLoading] = useState(true)
+    let [books, setBooks] = useState([])
+    let [deleteDialog, setDeleteDialog] = useState(false)
+    let [id, setID] = useState(null)
+    let [deleting, setDeleting] = useState(false)
+    let [deleted, setDeleted] = useState([false, '', 'success'])
+
+    const url = 'http://localhost:3300'
+
+    let getBooks = async () => {
+        let response = await fetch(url)
+        let data = await response.json()
+        setBooks(data)
+        setLoading(false)
+    }
+
+    useEffect(() => {
+        getBooks()
+    }, [])
+
+    let toggleDeleteDialog = () => setDeleteDialog(p => !p)
+
+    let deleteBook = async () => {
+        setDeleting(true)
+        let response = await fetch(url + '/books/' + id, { method: 'DELETE' })
+        setDeleting(false)
+        toggleDeleteDialog()
+        if (response.status === 201) {
+            setDeleted([true, 'Deleted Successfully!', 'success'])
+            getBooks()
+        }
+        else {
+            setDeleted([true, 'Something Went Wrong', 'error'])
+        }
+    }
+
+    return (
+        <Paper elevation={3} className={classes.paper}>
+            <Dialog onClose={toggleDeleteDialog} open={deleteDialog} className={classes.deleteDialog}>
+                <DialogTitle>Do you want to delete?</DialogTitle>
+                <div className="buttons" style={{ display: 'flex', gap: '1em', justifyContent: 'center', padding: '0em 1em 1em 1em' }}>
+                    <Button variant='contained' color='primary' onClick={deleteBook} disabled={deleting}>Yes</Button>
+                    <Button variant='contained' color='default' onClick={toggleDeleteDialog} disabled={deleting}>No</Button>
+                </div>
+            </Dialog>
+            <Snackbar open={deleted[0]} autoHideDuration={2000} onClose={() => setDeleted([false, '', 'success'])}>
+                <Alert elevation={6} variant="filled" onClose={() => setDeleted([false, '', 'success'])} severity={deleted[2]}>
+                 {deleted[1]}
+                </Alert>
+            </Snackbar>
+            <Typography align='center' variant='h5' className={classes.header}>List of Books</Typography>
+            {loading ? <CircularProgress />
+                : <List>
+                    {books.map((book) => <ListItem key={book._id}>
+                        <ListItemText primary={book.name} secondary={book.authors.join(' | ')} />
+                        <ListItemSecondaryAction>
+                            <IconButton edge="end" onClick={() => {
+                                toggleDeleteDialog()
+                                setID(book._id)
+                            }
+                            }>
+                                <Delete />
+                            </IconButton>
+                        </ListItemSecondaryAction>
+                    </ListItem>)}
+                </List>}
+        </Paper>
+    )
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,114 @@
+import { AppBar, Button, Container, CssBaseline, Drawer, IconButton, List, ListItem, ListItemIcon, ListItemText, ListSubheader, makeStyles, Toolbar, Typography } from "@material-ui/core";
+import { Book, Category, Menu, MenuBook, People } from "@material-ui/icons";
+import { useState } from "react";
+import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom'
+import BooksList from "./BooksList";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+    list: {
+        width: 250,
+    },
+    fullList: {
+        marginTop: theme.spacing(2),
+        width: 'auto',
+    },
+    menuHeader: {
+        color: 'inherit',
+        fontSize: '1.5rem',
+        marginBottom: theme.spacing(2)
+    },
+    container: {
+        display: 'flex',
+        justifyContent: 'center'
+    }
+}));
+
+
+export default function Main() {
+    return (
+        <Router>
+            <Wrapper>
+                <Switch>
+                    <Route exact path='/'>
+                        <h1>Welcome to Library</h1>
+                    </Route>
+                    <Route path='/books'>
+                        <BooksList />
+                    </Route>
+                    <Route path='/categories'>
+                        <h1>List of Categories</h1>
+                    </Route>
+                    <Route path='/members'>
+                        <h1>List of members</h1>
+                    </Route>
+                    <Route path='/issues'>
+                        <h1>List of Issued Books</h1>
+                    </Route>
+                </Switch>
+            </Wrapper>
+        </Router>
+    )
+
+}
+
+function Wrapper(props) {
+
+    const classes = useStyles();
+
+    let [drawerOpen, setDrawerOpen] = useState(false)
+
+    let toggleDrawer = () => {
+        setDrawerOpen(p => !p)
+    }
+
+    return (
+        <div className="container">
+            <CssBaseline />
+            <AppBar position='static'>
+                <Toolbar>
+                    <IconButton edge='start' className={classes.menuButton} color='inherit' onClick={toggleDrawer}>
+                        <Menu />
+                    </IconButton>
+                    <Typography variant='h6' className={classes.title}>
+                        McLaren College Library
+                    </Typography>
+                    <Button color='inherit'>Login</Button>
+                </Toolbar>
+            </AppBar>
+            <>
+                <Drawer anchor='left' open={drawerOpen} onClose={toggleDrawer}>
+                    <List className={classes.fullList}>
+                        <ListSubheader className={classes.menuHeader} component="div" >
+                            <Typography variant='h5' align='center' color='primary'>Menu</Typography>
+                        </ListSubheader>
+                        {[
+                            { text: 'Books', icon: <Book />, link: "/books" },
+                            { text: 'Categories', icon: <Category />, link: "/categories" },
+                            { text: 'Members', icon: <People />, link: "/members" },
+                            { text: 'Book Issued', icon: <MenuBook />, link: "/issues" },
+                        ].map(({ text, icon, link }) => (
+                            <Link key={text} to={link} style={{ textDecorationLine: 'none', color: 'black' }}>
+                                <ListItem button className={classes.list} onClick={toggleDrawer}>
+                                    <ListItemIcon>{icon}</ListItemIcon>
+                                    <ListItemText primary={text} />
+                                </ListItem>
+                            </Link>
+                        ))}
+                    </List>
+                </Drawer>
+            </>
+            <Container className={classes.container}>
+                {props.children}
+            </Container>
+        </div>
+    )
+}
